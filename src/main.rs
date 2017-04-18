5f55f96b8b2c6fe5f21360f5f43548c68a97e2b9
Karim Senhaji <karimair@gmail.com>
2017-04-18 23:33:08 +0200

    Welcome screen with proper terminal size

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ extern crate error_chain;
 extern crate nix;
 
 use std::io::Read;
+use std::io::Write;
 use nix::sys::termios;
+
+use nix::libc::STDIN_FILENO;
 
 mod errors {
     // Create the Error, ErrorKind, ResultExt, and Result types
@@ -17,8 +20,133 @@ mod errors {
 }
 use errors::*;
 
+// constants
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+// terminal
+struct TermConfig {
+    orig: termios::Termios,
+    rows: u16,
+    cols: u16,
+}
+
+fn ctrl(c: char) -> u8 {
+    (c as u8) & 0x1f
+}
+
+fn enable_raw_mode() -> Result<()> {
+    use termios::*;
+
+    let mut raw = tcgetattr(STDIN_FILENO)?;
+    raw.c_iflag.remove(BRKINT | ICRNL | INPCK | IXON);
+    raw.c_oflag.remove(OPOST);
+    raw.c_cflag.insert(CS8);
+    raw.c_lflag.remove(ECHO | ICANON | IEXTEN | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)?;
+
+    Ok(())
+}
+
+fn read_window_size() -> Result<(u16, u16)> {
+    use nix::libc::*;
+
+    unsafe {
+        let mut wc = winsize {
+            ws_row: 0,
+            ws_col: 0,
+            ws_xpixel: 0,
+            ws_ypixel: 0,
+        };
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &mut wc);
+        if wc.ws_col > 0 && wc.ws_row > 0 {
+            return Ok((wc.ws_row as u16, wc.ws_col as u16));
+        }
+    }
+    Err("Unable to read terminal size".into())
+}
+
+fn term_config() -> Result<TermConfig> {
+    let orig = termios::tcgetattr(STDIN_FILENO)?;
+    let (rows, cols) = read_window_size()?;
+    Ok(TermConfig {
+        orig: orig,
+        rows: rows,
+        cols: cols,
+    })
+}
+
+/** input **/
+fn process_key(b: u8) -> bool {
+    let c = b as char;
+    if b == ctrl('q') {
+        return false;
+    } else if c.is_control() {
+        print!("{}\r\n", b);
+    } else {
+        print!("{} ({})\r\n", b, c);
+    }
+    true
+}
+
+/** output **/
+fn draw_rows(rows: u16, cols: u16) {
+    let mut buf = String::new();
+    buf += "\x1b[?25l";             // Hide cursor
+    buf += "\x1b[H";                // Move cursor to top-right
+    for y in 0..rows {
+        if y == rows / 3 {
+            let welcome = format!("Welcome to Fanto editor version {}", VERSION);
+            let len = std::cmp::min(cols as usize, welcome.len());
+            let padding = (cols as usize - len) / 2;
+            if padding > 0 {
+                buf += "~";
+                buf += &std::iter::repeat(" ").take(padding - 1).collect::<String>();
+            }
+            buf += welcome.split_at(len - 1).0;
+        } else {
+            buf += "~";
+        }
+        buf += "\x1b[K";
+        if y < rows - 1 {
+            buf += "\r\n";
+        } 
+    }
+    buf += "\x1b[H";                // Move cursor to beginning
+    buf += "\x1b[?25h";             // Show cursor
+    print!("{}", buf);
+    let _ = std::io::stdout().flush();
+}
+
+fn refresh_screen() {
+    print!("\x1b[2J\x1b[H");
+    let _ = std::io::stdout().flush();
+}
+
+/** main function **/
+fn run() -> Result<()> {
+    let config = term_config().chain_err(|| "Unable to initialize terminal config")?;
+    draw_rows(config.rows, config.cols);
+    println!("rows: {}, cols: {}", config.rows, config.cols);
+
+    let stdin = std::io::stdin();
+    let mut bytes = stdin.lock().bytes();
+    enable_raw_mode()?;
+
+    while let Some(Ok(b)) = bytes.next() {
+        if !process_key(b) {
+            break;
+        }
+    }
+    println!();
+
+    termios::tcsetattr(STDIN_FILENO, termios::TCSAFLUSH, &config.orig)?;
+    Ok(())
+}
+
 fn main() {
-    if let Err(ref e) = run() {
+    let res = run();
+    refresh_screen();
+    if let Err(ref e) = res {
         use std::io::Write;
         let stderr = &mut ::std::io::stderr();
         let errmsg = "Error writing to stderr";
@@ -37,35 +165,4 @@ fn main() {
 
         ::std::process::exit(1);
     }
-}
-
-fn enable_raw_mode() -> Result<()> {
-    use termios::*;
-
-    let mut raw = tcgetattr(0)?;
-    raw.c_iflag.remove(BRKINT | ICRNL | INPCK | IXON);
-    raw.c_oflag.remove(OPOST);
-    raw.c_cflag.insert(CS8);
-    raw.c_lflag.remove(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(0, TCSAFLUSH, &raw)?;
-    Ok(())
-}
-
-fn run() -> Result<()> {
-    let origin = termios::tcgetattr(0)?;
-    let stdin = std::io::stdin();
-    let mut bytes = stdin.lock().bytes();
-    enable_raw_mode()?;
-
-    while let Some(Ok(b)) = bytes.next() {
-        match b as char {
-            'q' => break,
-            c if c.is_control() => print!("{}\r\n", b),
-            c => print!("{} ({})\r\n", b, c),
-        }
-    }
-    println!();
-
-    termios::tcsetattr(0, termios::TCSAFLUSH, &origin)?;
-    Ok(())
 }
